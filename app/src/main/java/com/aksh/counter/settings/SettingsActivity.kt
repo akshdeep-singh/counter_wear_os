273254eece1c0e79efd33b7ab47447c4bb5e2ee5
akshdeep-singh <aksh101099@gmail.com>
2021-09-10 14:43:33 +0530

    style

--- app/src/main/java/com/aksh/counter/settings/SettingsActivity.kt
+++ app/src/main/java/com/aksh/counter/settings/SettingsActivity.kt
@@ -5,16 +5,16 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 
-import com.aksh.counter.R
+import com.aksh.counter.databinding.ActivitySettingsBinding
 
 class SettingsActivity: Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_settings)
+        setContentView(ActivitySettingsBinding.inflate(layoutInflater).root)
     }
 
     fun View.onClickAlwaysOn()     = startActivity(AlwaysOnActivity::class.java)
-    fun View.onClickAnimation()    = startActivity(AnimationActivity::class.java)
+    // TODO fun View.onClickAnimation()    = startActivity(AnimationActivity::class.java)
     fun View.onClickButtons()      = startActivity(ButtonsActivity::class.java)
     fun View.onClickCounterCycle() = startActivity(CounterCycleActivity::class.java)
     fun View.onClickTextSize()     = startActivity(TextSizeActivity::class.java)
